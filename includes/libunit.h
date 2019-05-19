2c8996d568b312988c6ea099cbedc7c3e536dc69
Ivan Kozlov <ivaan.kozlov@gmail.com>
2019-05-19 16:36:06 -0700

    Removed colors from header

--- includes/libunit.h
+++ includes/libunit.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libunit.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: julee <marvin@42.fr>                       +#+  +:+       +#+        */
+/*   By: ivankozlov <ivankozlov@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/18 13:27:25 by julee             #+#    #+#             */
-/*   Updated: 2019/05/18 14:45:56 by julee            ###   ########.fr       */
+/*   Updated: 2019/05/19 04:19:06 by ivankozlov       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,10 +21,6 @@
 # define KO		(-1)
 # define SEGV	(11)
 # define BUSERR (10)
-
-# define RED	"\033[0;31m"
-# define GREEN	"\033[0;32m"
-# define BLUE	"\033[1;34m"
 
 typedef struct			s_unit_test
 {
