a57039ca7dc1ad6b3c7a22b5284f929ce85e5e72
Ivan Kozlov <ivaan.kozlov@gmail.com>
2019-05-19 16:35:29 -0700

    Added load test function

--- framework/load_test.c
+++ framework/load_test.c
@@ -6,12 +6,27 @@
 /*   By: ivankozlov <ivankozlov@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/19 01:39:04 by ivankozlov        #+#    #+#             */
-/*   Updated: 2019/05/19 02:32:17 by ivankozlov       ###   ########.fr       */
+/*   Updated: 2019/05/19 04:38:20 by ivankozlov       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libunit.h"
 
-int load_test()
+void	load_test(t_unit_test	**testlist, char *name, int (*f) (void))
 {
-	return (0);
+	t_unit_test		*new;
+	t_unit_test		*tmp;
+
+	new = ft_memalloc(sizeof(t_unit_test));
+	new->name = name;
+	new->f = f;
+	if (!(*testlist))
+		*testlist = new;
+	else
+	{
+		tmp = *testlist;
+		while (tmp->next)
+			tmp = tmp->next;
+		tmp->next = new;
+	}
 }
